c77f0ef20f21163f960abf70b64ea48f098b3631
Eswara Yerra <e.yerra@tricentis.com>
2022-02-08 20:43:47 -0500

    custom schema-registy image

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo 'Do Nothing'
+ACTION=$1
+TAG=$2
+
+if [[ $ACTION == "build" ]]; then
+    echo 'Do Nothing'
+elif [[ $ACTION == "docker-build" ]]; then
+    docker build -t $TAG
+fi
